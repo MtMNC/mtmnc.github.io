93829704ffb3c4bd2b85430c2dd57313603af91b
MtMNC <morris.mukau@gmail.com>
2016-08-30 21:11:30 -0700

    Pull box content from BS01, code improvements
    * Pull the content of the featured boxes from BS01
    * Improvements to the code that adds the background and featured box
    data to the page. It’s easier to expand upon, and it assumes less about
    the data given to it. (For example the “nodes” providing the data can
    be put in any order unlike before.)

--- bs01splash/alt/scripts/background.js
+++ bs01splash/alt/scripts/background.js
@@ -2,7 +2,7 @@ function getBackgroundData() {
   var backgroundApiCall = new XMLHttpRequest();
   backgroundApiCall.onreadystatechange = function() {
       if (backgroundApiCall.readyState == 4 && backgroundApiCall.status == 200) {
-          setBackground(backgroundApiCall.responseXML);
+          setPageInfo(backgroundApiCall.responseXML);
       }
   };
   //Since the HTML is on another domain (biosector01.com) and the HTML lacks the appropriate
@@ -10,52 +10,164 @@ function getBackgroundData() {
   //requesting the HTML itself, we let Yahoo! Query Language do the work for us. It'll process
   //the HTML, pull out the divs, then return them. Problem solved. Credit to Derek Gathright
   //on StackOverflow: http://stackoverflow.com/a/2679304
-  backgroundApiCall.open("GET", "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%27http%3A%2F%2Fbiosector01.com%2Fwiki%2Findex.php%3Ftitle%3DUser%3AMorris_the_Mata_Nui_Cow%2FSandbox2%26action%3Drender%27%20and%20xpath%3D%22%2F%2Fdiv%22", true);
+  backgroundApiCall.open("GET", "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D%22http%3A%2F%2Fbiosector01.com%2Fwiki%2Findex.php%3Ftitle%3DUser%3AMorris_the_Mata_Nui_Cow%2FSandbox2%26action%3Drender%22", true);
   backgroundApiCall.send();
 }
 
-function setBackground(response) {
+function setPageInfo(response) {
+  var nodes = response.getElementsByTagName("div"),
+      featuredNodes = [],
+      backgroundNodes = [];
+  //figure out whether each node is a featured node or a background node
+  for (i = 0; i < nodes.length; i++) {
+    if (featuredNodes.length < 2 && nodes[i].getAttribute("class") == "featured-node") {
+      //there can only be two featured nodes because there are only two customizable featured boxes
+      featuredNodes.push(nodes[i]);
+    } else if (nodes[i].getAttribute("class") == "background-node") {
+      backgroundNodes.push(nodes[i]);
+    }
+  }
+  if (backgroundNodes.length > 0) {
+    setBackground(backgroundNodes);
+  }
+  if (featuredNodes.length > 0) {
+    setFeatured(featuredNodes);
+  }
+}
+
+function setBackground(nodes) {
   var imageElement = document.getElementById("main-container"),
       exploreLinkElement = document.getElementById("explore-link"),
       exploreLinkNameElement = document.getElementById("explore-name"),
       gradientElement = document.getElementById("main-gradient");
-  //pick a random node (containing an image, link, and two colors) to use
-  var nodes = response.getElementsByTagName("div");
+
+  //Figure out which node to use.
+  //The number of nodes is at least 1, thanks to the setPageInfo() function.
+  //This means we can start processing knowing there's at least one background to do.
   var numberOfNodes = nodes.length;
   //pick random number between 1 and numberOfNodes (both inclusive)
   var randomNode = Math.floor(Math.random() * (numberOfNodes)) + 1;
   //the node count starts at 0, so subtract 1 from randomNode to get the node we want
   var nodeToUse = nodes[randomNode - 1];
 
-  //get the two subnodes, i.e. spans in the html, that are required (background image, explore link)
-  var imageSubnode = nodeToUse.firstChild,
-      exploreSubnode = imageSubnode.nextSibling;
-  //get the image's URL
-  var imageURL = imageSubnode.firstChild.getAttribute("href");
-  //set the body's background image to the image we just got
-  imageElement.style.backgroundImage = "url(" + imageURL + ")";
-  //get the explore link's URL and text
-  var exploreLinkURL = exploreSubnode.firstChild.getAttribute("href"),
-      exploreLinkText = exploreSubnode.firstChild.firstChild.nodeValue;
-  //set the explore link's URL and text to the values we just got
-  exploreLinkElement.href = exploreLinkURL;
-  exploreLinkNameElement.textContent = exploreLinkText;
-  exploreLinkElement.style.visibility = "visible";
+  //Go through all the subnodes and set the box's contents as appropriate.
+  //The 2 required subnodes are category, image, and link.
+  var imageAdded = false,
+      linkAdded = false;
+  //store the background image until we're done iterating, since it should stay hidden
+  //until we've determined there's an image and a link provided. Also it'll stop the image
+  //from changing multiple times if there are multiple image nodes.
+  var backgroundImage = null;
+  var subnode = null;
+  for (i = 0; i < nodeToUse.childElementCount; i++) {
+    subnode = nodeToUse.childNodes[i];
+    switch (subnode.getAttribute("class")) {
+      case "image":
+        //store the href of the anchor in the "image" subnode
+        backgroundImage = subnode.firstChild.getAttribute("href")
+        imageAdded = true;
+        break;
+      case "link":
+        //set the explore link's href to the href of the anchor in the "link" subnode
+        exploreLinkElement.href = subnode.firstChild.getAttribute("href");
+        //set the explore link's text to the text of the anchor in the "link" subnode
+        exploreLinkNameElement.textContent = subnode.firstChild.firstChild.nodeValue;
+        linkAdded = true;
+        break;
+      case "gradient":
+        //if there are not exactly two subnodes (one for each color) in this subnode, don't do anything to the gradient
+        if (subnode.childElementCount !== 2) {
+          break;
+        }
+        //get the colors for the background gradient
+        var color1 = subnode.firstChild.firstChild.nodeValue,
+            color2 = subnode.firstChild.nextSibling.firstChild.nodeValue;
+        //set the box's background gradient colors to the values we just got
+        gradientElement.setAttribute("style",
+                                     "background-color: " + color1 + ";" +
+                                     "background-image: -webkit-linear-gradient(135deg, " + color1 + ", " + color2 + ");" +
+                                     "background-image: -moz-linear-gradient(135deg, " + color1 + ", " + color2 + ");" +
+                                     "background-image: -o-linear-gradient(135deg, " + color1 + ", " + color2 + ");" +
+                                     "background-image: linear-gradient(135deg, " + color1 + ", " + color2 + ");"
+                                    );
+        break;
+    }
+  }
+  if (imageAdded && linkAdded) {
+    //if all required subnodes were provided
+    imageElement.style.backgroundImage = "url(" + backgroundImage + ")";
+    exploreLinkElement.style.visibility = "visible";
+  }
+}
 
-  //get the two optional subnodes (color 1, color 2) if they are both there, i.e. there are at least 4 subnodes
-  if (nodeToUse.childElementCount >= 4) {
-    var color1Subnode = exploreSubnode.nextSibling,
-        color2Subnode = color1Subnode.nextSibling;
-    //get the colors for the background gradient
-    var color1 = color1Subnode.firstChild.nodeValue,
-        color2 = color2Subnode.firstChild.nodeValue;
-    //set the background gradient's colors to the values we just got
-    gradientElement.setAttribute("style",
-                                 "background-color: " + color1 + ";" +
-                                 "background-image: -webkit-linear-gradient(135deg, " + color1 + ", " + color2 + ");" +
-                                 "background-image: -moz-linear-gradient(135deg, " + color1 + ", " + color2 + ");" +
-                                 "background-image: -o-linear-gradient(135deg, " + color1 + ", " + color2 + ");" +
-                                 "background-image: linear-gradient(135deg, " + color1 + ", " + color2 + ");"
-                                );
+function setFeatured(nodes) {
+  //The number of nodes is either 1 or 2, thanks to the setPageInfo() function.
+  //This means we can start processing knowing there's at least one box to do.
+  processFeaturedNode(nodes[0], "left");
+  if (nodes.length == 2) {
+    processFeaturedNode(nodes[1], "right");
+  }
+}
+
+function processFeaturedNode(node, side) {
+  var boxElement = document.getElementById("box-" + side),
+      linkElement = document.getElementById("link-" + side),
+      categoryElement = document.getElementById("category-" + side),
+      titleElement = document.getElementById("title-" + side),
+      subtitleElement = document.getElementById("subtitle-" + side),
+      gradientElement = document.getElementById("gradient-" + side);
+
+  //Go through all the subnodes and set the box's contents as appropriate.
+  //The 3 required subnodes are category, image, and link.
+  var categoryAdded = false,
+      imageAdded = false,
+      linkAdded = false;
+  var subnode = null;
+  for (i = 0; i < node.childElementCount; i++) {
+    subnode = node.childNodes[i];
+    switch (subnode.getAttribute("class")) {
+      case "category":
+        //set the box's category text (i.e., text on top) to the text in the "category" subnode
+        categoryElement.textContent = subnode.firstChild.textContent;
+        categoryAdded = true;
+        break;
+      case "image":
+        //set the box's background image to the href of the anchor in the "image" subnode
+        boxElement.style.backgroundImage = "url(" + subnode.firstChild.getAttribute("href") + ")";
+        imageAdded = true;
+        break;
+      case "link":
+        //set the box link's href to the href of the anchor in the "link" subnode
+        linkElement.href = linkURL = subnode.firstChild.getAttribute("href");
+        //set the box's title text (i.e., text above the image) to the text of the anchor in the "link" subnode
+        titleElement.textContent = subnode.firstChild.firstChild.nodeValue;
+        linkAdded = true;
+        break;
+      case "subtitle":
+        //set the box's subtitle text (i.e., smaller text below title text) to the text in the "subtitle" subnode
+        subtitleElement.textContent = subnode.firstChild.textContent;
+        break;
+      case "gradient":
+        //if there are not exactly two subnodes (one for each color) in this subnode, don't do anything to the gradient
+        if (subnode.childElementCount !== 2) {
+          break;
+        }
+        //get the colors for the background gradient
+        var color1 = subnode.firstChild.firstChild.nodeValue,
+            color2 = subnode.firstChild.nextSibling.firstChild.nodeValue;
+        //set the box's background gradient colors to the values we just got
+        gradientElement.setAttribute("style",
+                                     "background-color: " + color1 + ";" +
+                                     "background-image: -webkit-linear-gradient(135deg, " + color1 + ", " + color2 + ");" +
+                                     "background-image: -moz-linear-gradient(135deg, " + color1 + ", " + color2 + ");" +
+                                     "background-image: -o-linear-gradient(135deg, " + color1 + ", " + color2 + ");" +
+                                     "background-image: linear-gradient(135deg, " + color1 + ", " + color2 + ");"
+                                    );
+        break;
+    }
+  }
+  if (categoryAdded && imageAdded && linkAdded) {
+    //if all required subnodes were provided
+    linkElement.style.display = "inline-block";
   }
 }
