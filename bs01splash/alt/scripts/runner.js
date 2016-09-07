a20d9a5fd2f0940cbbd477011ec8d16f8a95f975
MtMNC <morris.mukau@gmail.com>
2016-09-06 18:21:59 -0700

    Fix JS

--- bs01splash/alt/scripts/runner.js
+++ bs01splash/alt/scripts/runner.js
@@ -2,14 +2,13 @@
 getBackgroundData();
 //getUserInfo();
 var searchSuggestions = new MWSearchSuggestions({"wiki": "http://biosector01.com/wiki/"});
-document.addEventListener("DOMContentLoaded", function(event) {
-  document.getElementById("sidebar-opener").addEventListener("click", function() {
-    document.body.classList.toggle("sidebar-open"); //IE 10+
-  });
-  document.getElementById("fade-overlay").addEventListener("click", function() {
-    document.body.classList.remove("sidebar-open"); //IE 10+
-  });
-  document.getElementById("sidebar-closer").addEventListener("click", function() {
-    document.body.classList.remove("sidebar-open"); //IE 10+
-  });
+
+document.getElementById("sidebar-opener").addEventListener("click", function() {
+  document.body.classList.toggle("sidebar-open"); //IE 10+
+});
+document.getElementById("fade-overlay").addEventListener("click", function() {
+  document.body.classList.remove("sidebar-open"); //IE 10+
+});
+document.getElementById("sidebar-closer").addEventListener("click", function() {
+  document.body.classList.remove("sidebar-open"); //IE 10+
 });
